3e401ffbd91d75538cba9e1dc884857e258110c0
Ivan Gerasimenko <noleen@ya.ru>
2022-08-18 01:03:15 +0300

    I love Kate

--- Ex005_If_Else/Program.cs
+++ Ex005_If_Else/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("enter userName");
+string username = Console.ReadLine();
+if(username.ToLower() == "kate")
+{
+    Console.WriteLine("I Love You, my Kate");
+}
+else
+{
+Console.WriteLine("Hello,");
+Console.WriteLine(username);
+}
