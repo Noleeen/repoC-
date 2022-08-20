d42a933c2ca0245b12e5578b0fd6bc632480effa
Ivan Gerasimenko <noleen@ya.ru>
2022-08-20 13:14:23 +0300

    function2

--- Lection2/Program.cs
+++ Lection2/Program.cs
@@ -1,6 +1,9 @@
 ﻿int Max(int arg1, int arg2, int arg3)
 {
-
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
 }
 
 int a1 = 15;
@@ -13,9 +16,22 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 323;
 
-int Max(a1, b1, c1)
+// int Max1 = Max(a1, b1, c1);  второй вариант при помощи функции
+// int Max2 = Max(a2, b2, c2);
+// int Max3 = Max(a3, b3, c3);
 
-/*int max = a1;
+// int MaxFin = Max(Max1, Max2, Max3);
+
+
+// третий вариант: преобразовали запись второго варианта
+int MaxFin = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+
+
+/*int max = a1;  перый вариант поиска наибольшего значения
 
 if (b1>max) max = b1;
 if(c1>max) max = c1;
@@ -29,4 +45,4 @@ if (b3>max) max = b3;
 if(c3>max) max = c3;
 */
 
-Console.Write(max1);
+Console.Write(MaxFin);
